88fe147af51ccc9fb0d3764524e65d0a8b9fe764
Gergely Havlicsek <hawle@users.noreply.github.com>
2016-07-21 18:04:02 +0200

    Fix php variables in up/down sql generation

--- src/SQLGen/DiffToSQL/InsertDataSQL.php
+++ src/SQLGen/DiffToSQL/InsertDataSQL.php
@@ -14,7 +14,7 @@ class InsertDataSQL implements SQLGenInterface {
         $values = $this->obj->diff['diff']->getNewValue();
         $values = array_map(function ($el) {
             if(!is_null($el)) {
-                return "'" . addslashes($el) . "'";
+                return "'" . preg_replace('/\\$/', '\\$', addslashes($el)) . "'";
             }
             else {
                 return 'NULL';
@@ -27,7 +27,7 @@ class InsertDataSQL implements SQLGenInterface {
         $table = $this->obj->table;
         $keys = $this->obj->diff['keys'];
         array_walk($keys, function(&$value, $column) {
-            $value = '`'.$column."` = '".addslashes($value)."'";
+            $value = '`'.$column."` = '".preg_replace('/\\$/', '\\$', addslashes($value))."'";
         });
         $condition = implode(' AND ', $keys);
         return "DELETE FROM `$table` WHERE $condition;";
